cced7f5346d95172789f9f436aba5bd577ca0371
Matthew Kenigsberg <matthewkenigsberg@gmail.com>
2021-10-04 13:58:47 -0500

    Add comments to main.sh

--- main.sh
+++ main.sh
@@ -12,25 +12,29 @@ function dump() {
   stat -f "${XFS_PATH}" > "stat${1}"
 }
 
-readarray -t PKGS < <(dnf repoquery --requires --resolve nginx)
+# setup directories
 rm -rf "${XFS_PATH}"/*
 mkdir "${RPMS}"
 mkdir "${REFLINKS}"
 dump 1
 
-# install
+# store nginx and all its dependencies in $PKGS
+readarray -t PKGS < <(dnf repoquery --requires --resolve nginx)
+
+# install each package into a separate directory under $RPMS
 for pkg in "${PKGS[@]}"; do
     rpm -i --nodeps --root "${RPMS}/${pkg}" $(dnf repoquery --location "${pkg}")
 done
 dump 2
 
-# reflinks
+# reflink each package's directory in $RPMS into $REFLINKS
 for pkg in "${PKGS[@]}"; do
   cp -r --reflink "${RPMS}/${pkg}" "${REFLINKS}/${pkg}"
 done
 dump 3
 
-# merge
+# create a hardlinked root filesystem tree by merging everything from each $pkg
+# directory in $REFLINKS to a single root filesystem directory in $MERGED
 for pkg in "${PKGS[@]}"; do
   rsync -a "${REFLINKS}/${pkg}/" "${MERGED}" --link-dest="${REFLINKS}/${pkg}"
 done
